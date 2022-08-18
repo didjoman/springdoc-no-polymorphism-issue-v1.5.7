1eeef0f59ae73c5a13805344229687a0814b33f0
Alexandre Rupp <alrupp-ext@airfrance.fr>
2022-08-18 18:18:31 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/controllers/CustomOpenApiWebMvcResource.java
@@ -0,0 +1,58 @@
+package com.example.demo.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.models.OpenAPI;
+
+import org.springdoc.core.AbstractRequestService;
+import org.springdoc.core.ActuatorProvider;
+import org.springdoc.core.GenericResponseService;
+import org.springdoc.core.OpenAPIService;
+import org.springdoc.core.OperationService;
+import org.springdoc.core.RepositoryRestResourceProvider;
+import org.springdoc.core.SecurityOAuth2Provider;
+import org.springdoc.core.SpringDocConfigProperties;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springdoc.webmvc.api.OpenApiWebMvcResource;
+import org.springdoc.webmvc.core.RouterFunctionProvider;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+import static org.springdoc.core.Constants.API_DOCS_URL;
+
+@RestController
+public class CustomOpenApiWebMvcResource extends OpenApiWebMvcResource {
+
+    public CustomOpenApiWebMvcResource(ObjectFactory<OpenAPIService> openAPIBuilderObjectFactory,
+        AbstractRequestService requestBuilder,
+        GenericResponseService responseBuilder,
+        OperationService operationParser,
+        RequestMappingInfoHandlerMapping requestMappingHandlerMapping,
+        Optional<ActuatorProvider> actuatorProvider,
+        Optional<List<OperationCustomizer>> operationCustomizers,
+        Optional<List<OpenApiCustomiser>> openApiCustomisers,
+        SpringDocConfigProperties springDocConfigProperties,
+        Optional<SecurityOAuth2Provider> springSecurityOAuth2Provider,
+        Optional<RouterFunctionProvider> routerFunctionProvider,
+        Optional<RepositoryRestResourceProvider> repositoryRestResourceProvider) {
+        super(openAPIBuilderObjectFactory, requestBuilder, responseBuilder, operationParser, requestMappingHandlerMapping, actuatorProvider, operationCustomizers, openApiCustomisers, springDocConfigProperties,
+            springSecurityOAuth2Provider, routerFunctionProvider, repositoryRestResourceProvider);
+    }
+
+    @Operation(hidden = true)
+    @GetMapping(value = "/doc", produces = MediaType.APPLICATION_JSON_VALUE)
+    public OpenAPI openapiJsonAsRealJson(HttpServletRequest request, @Value(API_DOCS_URL) String apiDocsUrl, Locale locale) {
+        calculateServerUrl(request, apiDocsUrl);
+        return this.getOpenApi();
+    }
+}
